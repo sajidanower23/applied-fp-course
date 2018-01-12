1551ee6a583629e60ce1ca5b1a22b8a75947bb48
Sajid Ibne Anower <sajid.anower@data61.csiro.au>
2018-01-12 17:55:51 +1100

    Implement app

--- level01/src/FirstApp/Main.hs
+++ level01/src/FirstApp/Main.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -fno-warn-unused-matches #-}
 module FirstApp.Main (runApp) where
 
-import           Network.Wai              --(Application, responseLBS)
+import           Network.Wai              -- (Application, Request, responseLBS, Response, ResponseReceived)
 import           Network.Wai.Handler.Warp (run)
 
 import           Network.HTTP.Types       (status200)
@@ -31,8 +31,8 @@ app
   :: Request
   -> (Response -> IO ResponseReceived)
   -> IO ResponseReceived
-app _ cb =
-  error "Application not implemented"
+app _ cb = cb $ responseLBS status200 [] "Hello internet"
+  -- error "Application not implemented"
 
 -- We keep this main function here as it is useful to build your application as
 -- a library. The reasoning behind this is that when you come to do your
@@ -40,5 +40,6 @@ app _ cb =
 -- needing to worry about any initialisation code you've buried in your
 -- executable Main.hs.
 runApp :: IO ()
-runApp = run undefined undefined
+runApp = run 8000 app
+
 
