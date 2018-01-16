4c10619a899484c5f5f7160edef3313d68ce634d
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-01-17 10:06:41 +1100

    Add definitions for constructors

--- level02/src/FirstApp/Types.hs
+++ level02/src/FirstApp/Types.hs
@@ -29,25 +29,25 @@ import           Data.Text       (Text)
 -- AddRq : Which needs the target topic, and the body of the comment.
 -- ViewRq : Which needs the topic being requested.
 -- ListRq : Which doesn't need anything and lists all of the current topics.
-data RqType
+data RqType = AddRq Topic CommentText
+            | ViewRq Topic
+            | ListRq
 
 -- Not everything goes according to plan, but it's important that our types
 -- reflect when errors can be introduced into our program. Additionally it's
 -- useful to be able to be descriptive about what went wrong.
 
 -- Fill in the error constructors as you need them.
-data Error
+data Error = EmptyTopic | EmptyComment
 
 -- Provide the constructors for a sum type to specify the `ContentType` Header,
 -- to be used when we build our Response type.
-data ContentType
+data ContentType = PlainText | JSON
 
 -- The ``ContentType`` constructors don't match what is required for the header
 -- information, so write a function that will take our ``ContentType`` and
 -- produce the correct value for the header.
-renderContentType
-  :: ContentType
-  -> ByteString
+renderContentType :: ContentType -> ByteString
 renderContentType =
   error "renderContentType not implemented"
 
@@ -82,23 +82,21 @@ newtype CommentText = CommentText Text
 mkTopic
   :: Text
   -> Either Error Topic
-mkTopic =
-  error "mkTopic not implemented"
+mkTopic "" = Left EmptyTopic
+mkTopic t = Right $ Topic t
 
 getTopic
   :: Topic
   -> Text
-getTopic =
-  error "getTopic not implemented"
+getTopic (Topic t) = t
 
 mkCommentText
   :: Text
   -> Either Error CommentText
-mkCommentText =
-  error "mkCommentText not implemented"
+mkCommentText "" = Left EmptyComment
+mkCommentText t = Right $ CommentText t
 
 getCommentText
   :: CommentText
   -> Text
-getCommentText =
-  error "getCommentText not implemented"
+getCommentText (CommentText t) = t
